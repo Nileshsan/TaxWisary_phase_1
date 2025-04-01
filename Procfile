8ced8d345e18e4fb5c15d38c123035d28b86be8a
Nilesh Sanyasi <nileshsanyasi02@gmail.com>
2025-04-01 14:35:00 +0530

    Initial commit for Render deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web: gunicorn tax_advisory.wsgi:application --bind 0.0.0.0:$PORT
+web: gunicorn tax_return_project.wsgi:application --bind 0.0.0.0:$PORT
+
+
--- tax_advisory/users/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import UserDocument
-
-class FileUploadForm(forms.ModelForm):
-    class Meta:
-        model = UserDocument
-        fields = ['file_name', 'file_type', 'file']
