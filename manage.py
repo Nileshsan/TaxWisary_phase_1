bfa4a68f69909a37e9832cfe99de42ed8e406ddb
Nilesh Sanyasi <nileshsanyasi02@gmail.com>
2025-04-01 20:09:11 +0530

    manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tax_advisory.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tax_return_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
